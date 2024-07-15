9b0470b7e3a0282f2c86a1e48f82736239e782e1
Raiana Ladeira <raianasancho@gmail.com>
2024-07-15 16:51:57 -0300

    Rename boards

--- components/actions.tsx
+++ components/actions.tsx
@@ -3,8 +3,9 @@
 import { api } from "@/convex/_generated/api";
 import { Id } from "@/convex/_generated/dataModel";
 import { useApiMutation } from "@/hooks/use-api-mutation";
+import { useRenameModal } from "@/store/use-rename-modal";
 import { DropdownMenuContentProps } from "@radix-ui/react-dropdown-menu";
-import { Link2, Trash2 } from "lucide-react";
+import { Link2, Pencil, Trash2 } from "lucide-react";
 import { toast } from "sonner";
 import { ConfirmModal } from "./confirm-modal";
 import { Button } from "./ui/button";
@@ -30,6 +31,7 @@ export const Actions = ({
   id,
   title,
 }: ActionsProps) => {
+  const { onOpen } = useRenameModal();
   const { mutate: remove, isLoading } = useApiMutation(api.board.remove);
 
   const onCopyLink = () => {
@@ -58,6 +60,15 @@ export const Actions = ({
           <Link2 className="h-4 w-4 mr-2" />
           Copy board link
         </DropdownMenuItem>
+
+        <DropdownMenuItem
+          onClick={() => onOpen(id, title)}
+          className="p-3 cursor-pointer"
+        >
+          <Pencil className="h-4 w-4 mr-2" />
+          Rename
+        </DropdownMenuItem>
+
         <ConfirmModal
           header="Delete board?"
           description="This will delete the board and all of its content"
